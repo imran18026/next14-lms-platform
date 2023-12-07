885a106e28ddb99de8b12b9b554f709e91cd1944
unknown <mdimranhossain.ist@gmail.com>
2023-12-07 11:05:13 +0600

    required change

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -1,12 +1,10 @@
 "use client";
-
 import axios from "axios";
 import MuxPlayer from "@mux/mux-player-react";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { Loader2, Lock } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { useConfettiStore } from "@/hooks/use-confetti-store";
 
